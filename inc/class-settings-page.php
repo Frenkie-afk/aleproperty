07302448f53366c250a5575c7ba40847ec43e536
Frenkie-afk <masterchannelmusic@gmail.com>
2025-01-17 15:01:46 +0200

    Registered new settings for settings page

--- inc/class-settings-page.php
+++ inc/class-settings-page.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * see: https://developer.wordpress.org/reference/hooks/plugin_action_links/#user-contributed-notes
+ * see: https://developer.wordpress.org/reference/functions/register_setting/
+ * see: https://developer.wordpress.org/reference/functions/add_settings_section/
+ * see: https://developer.wordpress.org/reference/functions/add_settings_field/
  *
 */
 
@@ -11,6 +14,7 @@ class AlepropertySettingsPage
 	{
 		add_action('admin_menu', [__CLASS__, 'add_setting_menu']);
 		add_filter( 'plugin_action_links', [__CLASS__, 'add_plugin_action_link'], 10, 2 );
+		add_action('admin_init', [__CLASS__, 'register_settings']);
 	}
 
 
@@ -20,7 +24,7 @@ class AlepropertySettingsPage
 			esc_html__('Aleproperty Settings', 'aleproperty'),
 			esc_html__('Aleproperty', 'aleproperty'),
 			'manage_options',
-			'aleproperty-settings',
+			'aleproperty-settings-page',
 			[__CLASS__, 'render_settings_page'],
 			'dashicons-admin-generic',
 			100
@@ -35,7 +39,7 @@ class AlepropertySettingsPage
 		$new_actions = [];
 
 		if ( basename( ALE_PROPERTY_PATH ) . '/Aleproperty.php'  === $plugin_file ) { //only for our plugin
-			$new_actions['aleproperty_settings'] = sprintf( __( '<a href="%s">Settings</a>', 'ale-property' ), esc_url( admin_url( 'admin.php?page=aleproperty-settings' ) ) );
+			$new_actions['aleproperty_settings'] = sprintf( __( '<a href="%s">Settings</a>', 'ale-property' ), esc_url( admin_url( 'admin.php?page=aleproperty-settings-page' ) ) );
 		}
 
 		return array_merge( $plugin_actions, $new_actions ); // merge arrays and return a new one
@@ -45,4 +49,74 @@ class AlepropertySettingsPage
 	{
 		require_once ALE_PROPERTY_PATH . '/templates/admin/settings.php';
 	}
+
+	public static function register_settings(): void
+	{
+
+		add_settings_section('aleproperty_setting_section', null, null, 'aleproperty-settings-page');
+
+		register_setting(
+			'aleproperty_settings',
+			'aleproperty_settings', // set also group name to save options as an array
+			[
+                'type' => 'array',
+                'sanitize_callback' => [__CLASS__, 'sanitize_array_settings'],
+            ]
+		);
+		add_settings_field(
+			'aleproperty_settings_title',
+			__('Title', 'ale-property'),
+			[__CLASS__, 'render_text_field'],
+			'aleproperty-settings-page',
+			'aleproperty_setting_section',
+            [   // $args
+                'full_name'     => 'aleproperty_settings[aleproperty_settings_title]',
+                'value'         =>  get_option('aleproperty_settings')['aleproperty_settings_title'] ?? '',
+            ]
+		);
+
+		add_settings_field(
+			'aleproperty_settings_button',
+			__('Show button', 'ale-property'),
+			[__CLASS__, 'render_checkbox_field'],
+			'aleproperty-settings-page',
+			'aleproperty_setting_section',
+			[   // $args
+				'full_name'     => 'aleproperty_settings[aleproperty_settings_button]',
+				'value'         =>  get_option('aleproperty_settings')['aleproperty_settings_button'] ?? 0,
+			]
+		);
+
+	}
+
+	public static function render_text_field($args): void
+	{
+        ?>
+		<input
+            type="text"
+            name="<?php echo esc_attr($args['full_name']); ?>"
+            value="<?php echo esc_attr( $args['value'] ); ?>"
+        >
+	<?php }
+
+	public static function render_checkbox_field($args): void
+	{
+		?>
+        <input
+            type="checkbox"
+            name="<?php echo esc_attr($args['full_name']); ?>"
+            value="1"
+            <?php checked($args['value'], 1); ?>
+        >
+	<?php }
+
+	public static function  sanitize_array_settings($input): array
+    {
+		if (!is_array($input)) {
+			return [];
+		}
+
+		return array_map('sanitize_text_field', $input); // sanitize all data
+	}
+
 }
--- templates/admin/settings.php
+++ templates/admin/settings.php
@@ -2,5 +2,15 @@
     <!-- Print the page title -->
     <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 
-    <div>CONTENT</div>
+    <form action="options.php" method="post">
+		<?php
+		settings_errors(); // call this function to see messages on submitting the form
+		settings_fields('aleproperty_settings'); // for passing wp security
+
+		// display previously registered sections for settings page with selected slug
+		do_settings_sections('aleproperty-settings-page');
+
+		submit_button();
+		?>
+    </form>
 </div>
